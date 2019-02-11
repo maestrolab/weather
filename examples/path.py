7c3e5315f32477eae635dbcbeb83f45e38a9dcd6
Jared Lilly <jplilly25@tamu.edu>
2019-02-11 09:51:08 -0600

    Added data for B787
    Data pickle and contour plot added for typecode B787

--- examples/path.py
+++ examples/path.py
@@ -1,5 +1,6 @@
 """
-Script to generate PDF of flight parameters for specified classes of aircraft.
+Script to generate PDF of velocity vs. angle of attack for specified classes of
+aircraft.
 """
 
 import pickle
@@ -33,7 +34,7 @@ class Airframe(object):
         """Read aircraft database csv."""
 
         type_structure = []
-        for n in range(8):
+        for n in range(2):
             type_structure.append('string')
 
         data = output_reader('../data/flight_plan/aircraftDatabases/' + filename, separator=[','],
@@ -41,12 +42,13 @@ class Airframe(object):
         return data
 
     def update_icao24s(self):
-        """Update list of icao24s for all typecodes."""
+        """Update list of icao24s for all typecodes.
+        Generates icao24s_self.timestamp.p pickle (all icao24s sorted by typecode)
+        """
 
         data = self.read_csv(self.csv_filename) # Made separate function in case a different filename
                           # is wanted to be used. (type(data) == dictionary)
 
-        # Include all Boeing and Airbus typecodes!!!!!!!!!
         typecodeDict = {'B737':{},'B747':{},'B757':{},'B767':{},'B777':{},'B787':{},
                         'A310':{},'A318':{},'A319':{},'A320':{},'A321':{},
                         'A330':{},'A340':{},'A350':{},'A380':{}}
@@ -57,10 +59,8 @@ class Airframe(object):
             i = 0
             icao24List = []
             for typecode in data['typecode']:
-                #print(typecode[0:3]) # Included in to check performance of filehandling.py
-                #print(key[0:3])
                 if typecode[0:3] == key[0:3]:
-                    icao24List.append(data['icao24'][i])
+                    icao24List.append(data['icao24'][i+1]) # i+1: lines up icao24 w/ typecode
                 i += 1
             typecodeDict[key]['icao24List'] = icao24List
 
@@ -70,14 +70,12 @@ class Airframe(object):
         icao24s.close()
 
     def update_OpenSkyApi(self):
-        """Description --> updates values for parameters 1 and 2"""
+        """Description --> updates values for velocity and angle of attack.
+        Generates v_aoa_pickle for self.typecode ('B737','B747',etc.)
+        """
 
         typecodeDict = pickle.load(open('../data/flight_plan/icao24s_'
                         + str(self.timestamp) + '.p','rb'))
-
-        # typecodeDict = self.update_icao24s()
-
-        print(len(typecodeDict[self.typecode]['icao24List']))
 
         for ica24 in typecodeDict[self.typecode]['icao24List']:
             api = OpenSkyApi('jplilly25','Crossfit25')
@@ -90,14 +88,32 @@ class Airframe(object):
                         self.vertrate = np.append(self.vertrate,
                                             np.array(state.states[0].vertical_rate))
             except:
-                print('Error occurred.')
+                print(ica24)
 
+        ################################################################################
+        # Attempting to speed up program by inputting a list of icao24s into get_states().
+        #   - First, invalid icao24s (len(icao24) != 6) are removed
+        #   - Then, the 'filtered' list is inputted into get_states()
+        #   - Problem: only three states are returned from get_states()
+        #
+        # i = 0
+        # while i < len(typecodeDict[self.typecode]['icao24List']):
+        #     if len(typecodeDict[self.typecode]['icao24List'][i]) != 6:
+        #         typecodeDict[self.typecode]['icao24List'] = np.append(typecodeDict[self.typecode]['icao24List'][:i],typecodeDict[self.typecode]['icao24List'][i+1:])
+        #     else:
+        #         i += 1
+        #
         # api = OpenSkyApi('jplilly25','Crossfit25')
         # state = api.get_states(time_secs=self.timestamp,icao24=typecodeDict[self.typecode]['icao24List'])
-        # if state.states:
-        #     self.velocity = np.array(state.states[0].velocity)
-        #     self.vertrate = np.array(state.states[0].vertical_rate)
+        # for n in range(len(state.states)):
+        #     if state.states:
+        #         self.velocity = np.append(self.velocity, state.states[n].velocity)
+        #         self.vertrate = np.append(self.vertrate, state.states[n].vertical_rate)
+        ################################################################################
 
+        ################################################################################
+        # Removing all instances where vertical_rate == None (assuming onground)
+        #
         # i = 0
         # while i < len(self.vertrate):
         #     if self.vertrate[i] == None:
@@ -105,12 +121,13 @@ class Airframe(object):
         #         self.velocity = np.append(self.velocity[:i], self.velocity[i+1:])
         #     else:
         #         i += 1
+        ################################################################################
 
         for n in range(len(self.velocity)):
             self.angleOfAttack = np.append(self.angleOfAttack,
                                     np.arctan(self.vertrate[n]/self.velocity[n]))
 
-        self.angleOfAttack = self.angleOfAttack*(180/np.pi)
+        self.angleOfAttack = self.angleOfAttack*(180/np.pi) # rad to degrees
         incidenceAngle = 6 # degrees
 
         self.angleOfAttack += incidenceAngle
@@ -122,9 +139,7 @@ class Airframe(object):
         icao24s.close()
 
     def generate_pdf(self):
-        """Description
-        **Included so that the pdf can be retrieved w/out plotting
-        """
+        """Description"""
 
         data = pickle.load(open('../data/flight_plan/v_aoa_pickles/icao24s_' +
                         str(self.typecode) + '_' + str(self.timestamp) + '.p','rb'))
@@ -138,12 +153,12 @@ class Airframe(object):
         return pdf
 
     def plot_pdf(self):
-        """Generate contour plot visualizing PDF of two parameters."""
+        """Generate contour plot visualizing PDF of velocity vs. angle of attack."""
 
         pdf = self.generate_pdf()
 
-        xgrid = np.linspace(np.amin(self.angleOfAttack), np.amax(self.angleOfAttack), 1000)
-        ygrid = np.linspace(np.amin(self.velocity), np.amax(self.velocity), 1000)
+        xgrid = np.linspace(0, 11, 1000)# np.amin(self.angleOfAttack), np.amax(self.angleOfAttack), 1000)
+        ygrid = np.linspace(0, 350, 1000)# np.amin(self.velocity), np.amax(self.velocity), 1000)
 
         X, Y = np.meshgrid(xgrid,ygrid)
         positions = np.vstack([X.ravel(), Y.ravel()])
@@ -152,7 +167,7 @@ class Airframe(object):
 
         fig = plt.figure()
         plt.contourf(X,Y,Z)
-        plt.title('PDF of Velocity vs. Angle of Attack')
+        plt.title('PDF of Velocity vs. Angle of Attack for %s' % self.typecode)
         plt.xlabel('Angle of Attack [degrees]')
         plt.ylabel('Velocity [m/s]')
         plt.colorbar()
@@ -166,7 +181,8 @@ if __name__ == '__main__':
                     'A310','A318','A319','A320','A321',
                     'A330','A340','A350','A380']
 
-    airFrame = Airframe(timestamp=1549729462,typecode=typecodeList[0])
-    airFrame.update_OpenSkyApi()
+    airFrame = Airframe(timestamp=1549729462,typecode=typecodeList[5])
+    # airFrame.update_icao24s()
+    airFrame.update_OpenSkyApi() # must run for each typecode before pdf generation
     # airFrame.generate_pdf()
     # airFrame.plot_pdf()
