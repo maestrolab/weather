8162db5cd60e35f435e59405e3b927d4c87576c4
Jonathan <jonrosen6@gmail.com>
2019-05-07 12:52:15 -0500

    updates for equivalent area method
    matlab_real_weather.py and matlab_standard.py now include a line defining the 'run_method' as panair or the equivalent area method. Later the script runs the correct method

--- examples/boom/standard/matlab_standard.py
+++ examples/boom/standard/matlab_standard.py
@@ -1,9 +1,10 @@
-from rapidboom import AxieBump
+from rapidboom import AxieBump, EquivArea
 from weather.boom import read_input
 from weather.scraper.twister import process_data
 import platform
 
 alt_ft = 50000.
+run_method = 'EquivArea' # or 'panair'
 
 # Collect input values
 bump_inputs = read_input('axie_bump_inputs.txt')
@@ -22,12 +23,18 @@ elif platform.system() == 'Windows':
 else:
     raise RuntimeError("platfrom not recognized")
 
-# Run
-# axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE) # for standard atmosphere
-axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE, altitude=alt_ft)
-axiebump.MESH_COARSEN_TOL = 0.00045
-axiebump.N_TANGENTIAL = 20
-loudness = axiebump.run(bump_inputs)
+if run_method == 'panair':
+	# Run (Panair)
+	axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE, altitude=alt_ft)
+	axiebump.MESH_COARSEN_TOL = 0.00045
+	axiebump.N_TANGENTIAL = 20
+	loudness = axiebump.run(bump_inputs)
+elif run_method == 'EquivArea':
+	# Run (equivalent area method)
+	axiebump = EquivArea(CASE_DIR, SBOOM_EXE, altitude=alt_ft)
+	loudness = axiebump.run(bump_inputs)
+else:
+	raise RuntimeError("evaluation method not recognized")
 
 print("Perceived loudness", loudness)
 
--- examples/boom/twister/matlab_real_weather.py
+++ examples/boom/twister/matlab_real_weather.py
@@ -1,7 +1,10 @@
-from rapidboom import AxieBump
+from rapidboom import AxieBump, EquivArea
 from weather.boom import read_input
 from weather.scraper.twister import process_data
 import platform
+
+
+run_method = 'EquivArea'
 
 # Bump design variables
 bump_inputs = read_input('axie_bump_inputs.txt')
@@ -21,31 +24,35 @@ f.close()
 # Collect input values
 atm_inputs = []
 for i in range(len(line)-1):
-    atm_inputs.append(float(line[i]))
+	atm_inputs.append(float(line[i]))
 # Extract input values
 if atm_inputs[0] < 10:
-    day = "0" + str(int(atm_inputs[0]))
+	day = "0" + str(int(atm_inputs[0]))
 else:
-    day = str(int(atm_inputs[0]))
+	day = str(int(atm_inputs[0]))
 if atm_inputs[1] < 10:
-    month = "0" + str(int(atm_inputs[1]))
+	month = "0" + str(int(atm_inputs[1]))
 else:
-    month = str(int(atm_inputs[1]))
+	month = str(int(atm_inputs[1]))
 year = str(int(atm_inputs[2]))
 if atm_inputs[3] == 0:
-    hour = "00"
+	hour = "00"
 elif atm_inputs[2] < 10:
-    hour = "0" + str(int(atm_inputs[3]))
+	hour = "0" + str(int(atm_inputs[3]))
 else:
-    hour = str(int(atm_inputs[3]))
+	hour = str(int(atm_inputs[3]))
 lat = atm_inputs[4]
 lon = atm_inputs[5]
 alt_ft = atm_inputs[6]
 
 # Extracting data from database
 alt_m = alt_ft * 0.3048
-data, altitudes = process_data(day, month, year, hour, alt_m,
-                               directory='../../../data/weather/twister/')
+try:
+	data, altitudes = process_data(day, month, year, hour, alt_m,
+								   directory='../../../data/weather/twister/')
+except(FileNotFoundError):
+	data, altitudes = process_data(day, month, year, hour, alt_m,
+								   directory='../../../../data/weather/twister/')
 key = '%i, %i' % (lat, lon)
 weather_data = data[key]
 
@@ -59,21 +66,25 @@ SBOOM_EXE = 'sboom_windows.dat.allow.exe'  # name of the sboom executable
 
 print(platform.system())
 if platform.system() == 'Linux' or platform.system() == 'Darwin':
-    PANAIR_EXE = 'panair'
-    SBOOM_EXE = 'sboom_linux'
+	PANAIR_EXE = 'panair'
+	SBOOM_EXE = 'sboom_linux'
 elif platform.system() == 'Windows':
-    PANAIR_EXE = 'panair.exe'
-    SBOOM_EXE = 'sboom_windows.dat.allow'
+	PANAIR_EXE = 'panair.exe'
+	SBOOM_EXE = 'sboom_windows.dat.allow'
 else:
-    raise RuntimeError("platfrom not recognized")
+	raise RuntimeError("platfrom not recognized")
 
 # Run
-axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE,
-                    altitude=height_to_ground,
-                    weather=weather_data)
-axiebump.MESH_COARSEN_TOL = 0.00045
-axiebump.N_TANGENTIAL = 20
-loudness = axiebump.run(bump_inputs)
+if run_method == 'panair':
+	axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE, altitude=alt_ft)
+	axiebump.MESH_COARSEN_TOL = 0.00045
+	axiebump.N_TANGENTIAL = 20
+	loudness = axiebump.run(bump_inputs)
+elif run_method == 'EquivArea':
+	axiebump = EquivArea(CASE_DIR, SBOOM_EXE, altitude=alt_ft)
+	loudness = axiebump.run(bump_inputs)
+else:
+	raise RuntimeError("evaluation method not recognized")
 
 print("Perceived loudness", loudness)
 
