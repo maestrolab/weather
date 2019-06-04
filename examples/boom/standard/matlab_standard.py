d806cd47e329603782bc2b77fd30f9c7fd0eeb36
jonrosen6 <jonrosen6@gmail.com>
2019-06-04 12:16:30 -0500

    Update matlab_standard.py
    - to call the new version of read_input
    - also suppressed some unnecessary lines

--- examples/boom/standard/matlab_standard.py
+++ examples/boom/standard/matlab_standard.py
@@ -4,14 +4,14 @@ from weather.scraper.twister import process_data
 import platform
 
 alt_ft = 50000.
-run_method = 'EquivArea' # or 'panair'
+#run_method = 'EquivArea' # or 'panair'
 
 # Collect input values
-bump_inputs = read_input('axie_bump_inputs.txt', n=5)
+deformation, run_method, bump_inputs = read_input('axie_bump_inputs.txt')
 
 CASE_DIR = "./"  # axie bump case
-PANAIR_EXE = 'panair.exe'  # name of the panair executable
-SBOOM_EXE = 'sboom_windows.dat.allow.exe'  # name of the sboom executable
+#PANAIR_EXE = 'panair.exe'  # name of the panair executable
+#SBOOM_EXE = 'sboom_windows.dat.allow.exe'  # name of the sboom executable
 
 print(platform.system())
 if platform.system() == 'Linux' or platform.system() == 'Darwin':
@@ -25,13 +25,13 @@ else:
 
 if run_method == 'panair':
 	# Run (Panair)
-	axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE, altitude=alt_ft, deformation='cubic')
+	axiebump = AxieBump(CASE_DIR, PANAIR_EXE, SBOOM_EXE, altitude=alt_ft, deformation=deformation)
 	axiebump.MESH_COARSEN_TOL = 0.00045
 	axiebump.N_TANGENTIAL = 20
 	loudness = axiebump.run(bump_inputs)
 elif run_method == 'EquivArea':
 	# Run (equivalent area method)
-	axiebump = EquivArea(CASE_DIR, SBOOM_EXE, altitude=alt_ft, deformation='cubic')
+	axiebump = EquivArea(CASE_DIR, SBOOM_EXE, altitude=alt_ft, deformation=deformation)
 	loudness = axiebump.run(bump_inputs)
 else:
 	raise RuntimeError("evaluation method not recognized")
