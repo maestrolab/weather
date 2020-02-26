Added PLdB gradients and fixed elevation for matlab code

data/noise/convert_to_matlab.py
@@ -4,31 +4,34 @@ convert dos linefeeds (crlf) to unix (lf)
 usage: dos2unix.py 
 """
 import pickle
-import numpy, scipy.io
+import numpy as np
+import scipy.io
 
-filename = '20180618_12_50000'
+filename = '20180621_12_50000'
 data = pickle.load(open(filename + '.p', 'rb'))
-key_list = list(data.keys())
+n = 46*92
+key_list = range(n)
 properties = ['temperature', 'pressure', 'humidity']
 data_list = {}
 
 # LatLon values
-latlon_data = []
-for key in key_list:
-    latlon_data.append([float(x) for x in key.split(',')])
-data_list['latlon'] = latlon_data
+data_list['latlon'] = data.lonlat[:,::-1]
+height = data.height.reshape([31, n]).T
+temperature = data.temperature.reshape([31, n]).T
+humidity = data.humidity.reshape([31, n]).T
+# pressure = data.pressure.reshape([31, n])
+wind_x = data.wind_x.reshape([31, n]).T
+wind_y = data.wind_y.reshape([31, n]).T
 
 # Storing weather data
-
-for property in properties:
-    data_list[property] = []
-for key in key_list:
-    for property in properties:
-        data_list[property].append([data[key][property]])
+data_list['temperature'] = temperature
+data_list['humidity'] = humidity
+# data_list['pressure'] = np.array([height, pressure]).T
+data_list['wind_x'] = wind_x
+data_list['wind_y'] = wind_y
+data_list['height'] = height
+data_list['noise'] = data.noise
+data_list['elevation'] = np.array(data.elevation).flatten().T
 
 # Storing noise data
-noise_list = []
-for key in key_list:
-    noise_list.append([data[key]['noise']])
-data_list['noise'] = noise_list
 scipy.io.savemat('./' + filename + '.mat', data_list)

examples/data_processing/noaa/gradient_contour.py
@@ -2,13 +2,14 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from numpy import linalg as LA
 import pickle
 
 from weather.scraper.noaa import process
 year = '2018'
 month = '06'
 day = '21'
-hour = '00'
+hour = '12'
 alt_ft = 50000
 directory = '../../../data/noise/'
 filename = directory + year + month + day + '_' + hour + '_' + \
@@ -27,11 +28,16 @@ m.drawstates()
 m.drawcountries(linewidth=1.0)
 m.drawcoastlines()
 
-plt.contourf(map_lon, map_lat, np.array(data.noise).reshape(data.lon_grid.shape),
-             cmap=cm.coolwarm, levels=np.linspace(75, 87, 7))
+noise = np.array(data.noise).reshape(data.lon_grid.shape)
+gradients = LA.norm(np.gradient(noise), axis=0)
+print(noise)
+print(gradients)
+print(np.max(gradients), np.min(gradients))
+plt.contourf(map_lon, map_lat, gradients,
+             cmap=cm.coolwarm, levels=np.linspace(0, 2, 9))
 
 cbar = m.colorbar()
 degree_sign = '\N{DEGREE SIGN}'
-label = "Perceived level in dB (PLdB)"
+label = "PLdB Gradient magnitude"
 cbar.set_label(label)
 plt.show()